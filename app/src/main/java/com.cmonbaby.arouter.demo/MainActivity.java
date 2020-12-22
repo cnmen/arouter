2b038ec2723bc876e43ccc7bf5a97e8856b80415
Simon <simon@cmonbaby.com>
2020-12-22 13:54:05 +0800

    support_v7 to androidx

--- app/src/main/java/com.cmonbaby.arouter.demo/MainActivity.java
+++ app/src/main/java/com.cmonbaby.arouter.demo/MainActivity.java
@@ -2,8 +2,8 @@ package com.cmonbaby.arouter.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
--- library/src/main/java/com/cmonbaby/arouer/demo/library/LibraryActivity.java
+++ library/src/main/java/com/cmonbaby/arouer/demo/library/LibraryActivity.java
@@ -1,6 +1,6 @@
 package com.cmonbaby.arouer.demo.library;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
